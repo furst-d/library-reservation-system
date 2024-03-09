fc7ebadb55e2ad9bd309d2e8317b1e8029795e1f
Dominik Fűrst <dominikfurst1@gmail.com>
2024-03-09 15:32:41 +0100

    Renamed class

---
+++ web_api/src/main/java/org/furstd/web_api/model/util/Msg.java
@@ -0,0 +1,10 @@
+package org.furstd.web_api.model.util;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Msg {
+    private final String message;
+}
